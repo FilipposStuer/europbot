14a26c9e5190dcae4c97ebc8baa05a0ddcaf5bff
FilipposStuer <40569725+FilipposStuer@users.noreply.github.com>
2018-07-02 13:39:55 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -6,36 +6,38 @@ app = Flask(__name__)
 
 while True:
 
-    api = CiscoSparkAPI(access_token="OTg5MTk2ODYtYWM1My00YzNlLTgwYTItYzcwOGIzY2YxZDAyZWQxNGI4ODMtMTRm")
+    api = CiscoSparkAPI(access_token="ZDliZDdkYjMtNDljNS00MWQ4LThiMTItZjYwZmY1NmQwZTQ0YjM0NjAzNWQtZWIz")
     conversations = api.rooms.list(max=(1),sortBy="lastactivity")
     conversations = list(conversations)
     conversations = conversations[0]
     person = api.people.get(personId="{0}".format(conversations.creatorId))
-    messages = api.messages.list(roomId="{0}".format(conversations.id))
+    messages = api.messages.list(roomId="Y2lzY29zcGFyazovL3VzL1JPT00vMjQ3ZWZiMmUtODhjZS0zOTgyLWEzZWQtMzQyZTliNzRhNzYx")
+
     message = list(messages)
     last_message = message[0]
     information = api.people.get(personId="{0}".format(conversations.creatorId))
     persons_id = information.orgId
-    if last_message.text == "help":
-        api.messages.create(toPersonId="{0}".format(conversations.creatorId), text="The commands are as follows: Hello,What is my IDS ")
+
+    if last_message.text == "Help":
+        api.messages.create(toPersonId="{0}".format(conversations.creatorId), text="The commands are as follows: Hello , What is my ID and Create room")
     if last_message.text == "hello" or last_message.text == "Hello" or last_message.text == "Hi":
         api.messages.create(toPersonId="{0}".format(conversations.creatorId), text="Hello, what can I help you with(Type help for a list of comands)")
     if last_message.text == "What is my ID"or last_message.text == "What is my Id" or last_message.text == "What is my id":
         api.messages.create(toPersonId="{0}".format(conversations.creatorId), text="Your id is:")
         api.messages.create(toPersonId="{0}".format(conversations.creatorId), text="{0}".format(persons_id))
     if last_message.text == "Create room":
-        n=0
         api.messages.create(toPersonId="{0}".format(conversations.creatorId),text="What should the Room be called?")
-
-        while n==0 :
-            messages = api.messages.list(roomId="{0}".format(conversations.id))
-            message = list(messages)
-            last_message = message[0]
-            n=last_message.text
-            print(n)
-            time.sleep(500000000)
-        print(n)
-        api.messages.create(toPersonId="{0}".format(conversations.creatorId,text="{}hjhkjf"))
+        time.sleep(10)
+        messages = api.messages.list(roomId="{0}".format(conversations.id))
+        message = list(messages)
+        last_message = message[0]
+        print(type(last_message.text))
+        api.messages.create(toPersonId="{0}".format(conversations.creatorId),text="{0}".format(last_message.text))
+        print(last_message.text)
+        room=api.teams.create(name="{0}".format(last_message.text))
+        api.team_memberships.create(teamId="{0}".format(room.id),personId="{0}".format(conversations.creatorId),isModerator=True)
+     #if last_message == "Add person to team ":
+         #api.messages.create(toPersonId="{}".format(conversations.creatorId, text="What group should i add on"))
     time.sleep(1)
 
 
